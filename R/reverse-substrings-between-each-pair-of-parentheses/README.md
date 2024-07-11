2a52f32e4458495b5ef0017b42505b76bc9f7688
Francisco <fmartinezv2002@gmail.com>
2024-07-11 04:52:05 -0400

    Daily Leetcode Problem

---
+++ R/reverse-substrings-between-each-pair-of-parentheses/README.md
@@ -0,0 +1,5 @@
+# Reverse Substrings Between Each Pair of Parentheses
+
+## Solutions
+### Stack 
+[Vue](https://github.com/vuejs/vue)
---
+++ R/reverse-substrings-between-each-pair-of-parentheses/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> openParenthesesIndices;
+        string result = "";
+
+        for (char currentChar : s) {
+            if (currentChar == '(') {
+                // Store the current length as the start index for 
+                // a future reversal from (start, end)
+                openParenthesesIndices.push(result.length());
+            }
+            else if (currentChar == ')') {
+                int start = openParenthesesIndices.top();
+                openParenthesesIndices.pop();
+                // Reverse the substring between the matching parentheses
+                reverse(result.begin() + start, result.end());
+            }
+            else {
+                // Append non-parenthesis characters to the string
+                result += currentChar;
+            }
+        }
+        return result;
+    }
+};
